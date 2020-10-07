abdfc7240e997598bef10eae57c8320294089ad4
Celio <celiocazares99@gmail.com>
2020-10-07 17:04:01 -0300

    Actions template

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,7 +1,22 @@
-const fetchUsers = response => ({
-    type: 'FETCH_USERS',
-    response
-  })
+import { usersActionTypes } from "../actionTypes/usersActionTypes";
+import { usersHandler } from "../handlers/usersHandler";
+
+const fetchUsers = () => {
+  return dispatch => {
+    dispatch(request());
+    usersHandler.getAll()
+      .then(
+        users => dispatch(success(users)),
+        error => { console.log(error) }
+      ).catch(error => {
+        dispatch(failure(error));
+      });
+  };
+
+  function request() { return { type: usersActionTypes.FETCH_USERS_REQUEST } }
+  function success(users) { return { type: usersActionTypes.FETCH_USERS_SUCCESS, users } }
+  function failure(error) { return { type: usersActionTypes.FETCH_USERS_FAILURE, error } }
+}
 
 const addUsers = response => ({
   type: 'ADD_USER',
@@ -12,3 +27,9 @@ export const usersActions = {
   fetchUsers,
   addUsers
 };
+
+
+// const addUsers = response => ({
+//   type: 'FETCH_USERS',
+//   response
+// })
