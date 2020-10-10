7e708a4e5fac13d3b6c45f9a9f1bbf48bba04663
Celio <celiocazares99@gmail.com>
2020-10-10 14:19:04 -0300

    Added styled components

--- src/Pages/users/login.js
+++ src/Pages/users/login.js
@@ -3,11 +3,33 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { authenticationActions } from "../../actions/authenticationActions"
 
+//STYLE
+import styled from 'styled-components'
+
 const Login = () => {
   const [userName, setUser] = useState('');
   const [userPassword, setPassword] = useState('');
   const dispatch = useDispatch();
   const history = useHistory()
+
+
+  const Container = styled.div`
+    display: grid;
+    grid-template-rows: 20vh 70vh 10vh;
+    text-align:center;
+  `
+
+  const Header = styled.header`
+    // background-color: red
+  `
+
+  const InputBox = styled.div`
+    // margin: auto 0
+  `
+
+  const Footer = styled.footer`
+    // background-color: blue
+  `
 
   const loginCallBack = (response) => {
     history.push('/users')
@@ -25,16 +47,21 @@ const Login = () => {
   console.log('userPassword', userPassword)
 
   return (
-    <>
-      <h1>Login</h1>
-      <div>
+    <Container>
+      <Header>
+        Login
+      </Header>
+      <InputBox>
         <input type="text" value={userName} onChange={event => setUser(event.target.value)} placeholder="Username" />
         <input type="password" value={userPassword} onChange={event => setPassword(event.target.value)} placeholder="Password" />
-      </div>
-      <div>
-        <button onClick={login}>Login</button>
-      </div>
-    </>
+        <div>
+          <button onClick={login}>Login</button>
+        </div>
+      </InputBox>
+      <Footer>
+        FOOTER
+      </Footer>
+    </Container>
   )
 }
 
